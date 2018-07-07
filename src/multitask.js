0b193e3bf2dd2781e4ea6b19c1b9fd4cc1ee59ba
Padam <sandhp7@gmail.com>
2018-07-06 21:32:52 -0400

    Question

--- src/multitask.js
+++ src/multitask.js
@@ -5,14 +5,15 @@ const fs = require('fs');
 const start = Date.now();
 
 function doRequest() {
-https.request("https://www.bell.ca", res => {
+https.request("https://www.google.ca", res => {
     res.on('data', () => {});
     res.on('end', () => {
-        console.log(Date.now() - start);
+        console.log("Sent Request",Date.now() - start);
     });
 })
 .end()
 }
+
 
 function doHash() {
     crypto.pbkdf2('a','b', 100000, 512 ,'sha512', () => {
@@ -20,3 +21,35 @@ function doHash() {
     });
 }
 
+doRequest();
+
+fs.readFile('multitask.js','utf8', () => {
+    console.log('FS', Date.now() - start);
+});
+
+doHash();
+doHash();
+doHash();
+doHash();
+
+
+// ---------     Comments    ---------
+/**
+|--------------------------------------------------
+ 1: When we run this file, we are going to make request to google!!
+ 2: Than going to attempt to read all the contents in multitask.js file.
+ 3: Than going to calculate 4 Hashes.
+
+        So Million dollar question is in what order your are going to see
+        these console.log are going to display  ??
+
+Answer:
+
+Sent Request 861
+Hash: 1918FS 1919
+Hash: 1920
+Hash: 1929
+Hash: 1931
+
+|--------------------------------------------------
+*/
